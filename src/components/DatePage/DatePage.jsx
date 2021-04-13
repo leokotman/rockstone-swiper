958728d05b1fa933a1f3f54ef4017d8c5a4f1beb
levkotman <leoktmn@yandex.ru>
2021-04-14 01:59:40 +0200

    styled datepage

--- src/components/DatePage/DatePage.jsx
+++ src/components/DatePage/DatePage.jsx
@@ -1,31 +1,30 @@
-import React  from 'react';
+import style from "./DatePage.module.css";
+import React from "react";
 
 class DatePage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      time: new Date().toLocaleString()
+      time: new Date().toLocaleString(),
     };
   }
   componentDidMount() {
-    this.intervalID = setInterval(
-      () => this.tick(),
-      1000
-    );
+    this.intervalID = setInterval(() => this.tick(), 1000);
   }
   componentWillUnmount() {
     clearInterval(this.intervalID);
   }
   tick() {
     this.setState({
-      time: new Date().toLocaleString()
+      time: new Date().toLocaleString(),
     });
   }
   render() {
     return (
-      <p className="App-clock">
-        The time is {this.state.time}.
-      </p>
+      <div className={style.date_page}>
+        <h1>Current time is: </h1>
+        <span>{this.state.time}</span>
+      </div>
     );
   }
 }
