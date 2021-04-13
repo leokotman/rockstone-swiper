518f59c31ba39ff0f98ae34c9e4c9eed1c034bc0
levkotman <leoktmn@yandex.ru>
2021-04-13 21:14:31 +0200

    added react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Carousel} from "react-bootstrap";
+
 function App() {
   return (
     <div className="App">
@@ -9,6 +12,30 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Carousel interval={null}>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=First slide&bg=373940"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=Second slide&bg=282c34"
+              alt="Second slide"
+            />
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
         <a
           className="App-link"
           href="https://reactjs.org"
